5ef166be37fb82d1a7a23054f109e99b20cc9d78
Crowdbotics <team@crowdbotics.com>
2020-07-24 17:53:47 +0000

    Update models

--- backend/lizz_jul21v3_mob1_19080/wsgi.py
+++ backend/lizz_jul21v3_mob1_19080/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_jul21v3_mob1_19080.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lizz_jul21v3_mob1_19080.settings")
 
 application = get_wsgi_application()
